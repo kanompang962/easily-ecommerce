a650735943c69379e46ceb0c3b6e5436e6996387
tiencahi chamnan <kanompang962@gmail.com>
2023-02-19 22:56:09 +0700

    navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,59 @@
 import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+import { BsSearch } from 'react-icons/bs'
 
 function Header() {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <div>
+            <header className='header-top-strip py-3'>
+                <div className='container-xxl'>
+                    <div className='row'>
+                        <div className='col-6'>
+                            <p className='text-white mb-0'>
+                                Free Shipping Over $100 & Free Returns
+                            </p>
+                        </div>
+                        <div className='col-6'>
+                            <p className='text-end text-white mb-0'>
+                                Hotline:
+                                <a className='text-white' href='tel:+91 8264954234'>
+                                    +91 8264954234
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <header className='header-upper py-3'>
+                <div className='container-xxl'>
+                    <div className='row'>
+                        <div className='col-2'>
+                            <h2>
+                                <Link className='text-white'>Dev Corner</Link>
+                            </h2>
+                        </div>
+                        <div className='col-5'>
+                            <div class="input-group mb-3">
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    placeholder="Search Product Here.."
+                                    aria-label="Search Product Here.."
+                                    aria-describedby="basic-addon2" />
+                                <span class="input-group-text" id="basic-addon2">
+                                    <BsSearch />
+                                </span>
+                            </div>
+                        </div>
+                        <div className='col-5'>
+
+                        </div>
+                    </div>
+                </div>
+            </header>
+
+        </div>
+    )
 }
 
 export default Header
